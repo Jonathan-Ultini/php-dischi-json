cad3233a1d09905605fe0307591f23650d47c690
Jonathan-Ultini <jonathanultini@gmail.com>
2024-10-08 16:01:47 +0200

    call API + frontend insertion base

--- index.php
+++ index.php
@@ -5,10 +5,40 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Dischi JSON</title>
+  <link rel="stylesheet" href="styles/style.css">
+  <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 
 <body>
-  <h1>PHP Dischi JSON</h1>
+  <div id="app">
+    <div class="album" v-for="album in albums" :key="album.title">
+      <img :src="album.poster" :alt="album.title">
+      <h3>{{ album.title }}</h3>
+      <p>{{ album.author }}</p>
+      <p><b>{{ album.year }}</b></p>
+      <!-- <p>{{ album.genre }}</p> -->
+    </div>
+  </div>
+
+  <script>
+  new Vue({
+    el: '#app',
+    data: {
+      albums: []
+    },
+    mounted() {
+      // Chiamata API per ottenere i dischi
+      axios.get('dischi.php')
+        .then(response => {
+          this.albums = response.data;
+        })
+        .catch(error => {
+          console.error("Errore nel caricamento dei dati:", error);
+        });
+    }
+  });
+  </script>
 </body>
 
 </html>
